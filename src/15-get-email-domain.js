0ee97b0ff5b46ae650ace591559b417246eea72c
Dovoyan <70629154+Dovoyan@users.noreply.github.com>
2021-04-11 20:54:54 +0300

    feat: make 15

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,15 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  // const dog = '@';
+  // let z = 0;
+  // let em = email.split('');
+  // for (let i = 0; i < em.length; i++) {
+  //   if (em[i] === dog)z = i;
+  // }
+  const arr = email.split('@');
+  return arr[arr.length - 1];
 }
 
 module.exports = getEmailDomain;
